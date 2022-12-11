873371a38c6a25c6da599ecec031f5f15d2002e4
Xinsong Lin <zhongjiezhex12@me.com>
2022-12-11 15:21:52 -0800

    wrote down the specs

---
+++ scripts/permalink.js
@@ -0,0 +1,10 @@
+/** @module permalink */
+
+/**
+ * basically the "toString" method for the graph
+ * @param {Object} graph - check "graph_components.js" for the structure of the graph
+ * @returns {string} the graph as a string
+ */
+export function serialize(graph) {
+  
+}
