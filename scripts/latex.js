208ad432df20f591160364284f6e9b1e1707aa55
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-11-20 17:53:02 -0800

    added base string

--- scripts/latex.js
+++ scripts/latex.js
@@ -16,5 +16,19 @@
  * @return string representation of graph in latex tikzpicture
  */
 export function serialize(graph) {
-    return "Hello World";
+    // setup
+    let output = "\\begin{tikzpicture}[->,>=stealth',shorten >=1pt, auto, node distance=2cm, semithick]\n";
+    output += "\\tikzstyle{every state}=[text=black, fill=none]\n";
+
+    const vertices = Object.values(graph);
+
+    vertices.forEach((v) => {
+        let inner = 'state,';
+        if(v.is_start) inner += 'initial,';
+        if(v.is_final) inner += 'accepting';
+
+        output += `\\node[${inner}] (${v.name}) {${v.name}}`;
+    });
+
+    console.log(output);
 }
